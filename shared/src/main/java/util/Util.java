2af01082f4613a69cf83063053e84195f0955dc1
Noah Pratt <prattnj@gmail.com>
2024-02-21 13:17:28 -0700

    Update Util.java

--- shared/src/main/java/util/Util.java
+++ shared/src/main/java/util/Util.java
@@ -14,7 +14,7 @@ public class Util {
      * Represents the current database being used. Currently, supports "ram" and "mysql".
      * Can be overridden when running server.Server
      */
-    public static String DB_TYPE = "mysql";
+    public static String DB_TYPE = "ram";
 
     public static String INVALID_TOKEN = "invalid authToken";
 
