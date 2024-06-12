dc7065f31abb6b71426ac93fe93792643ef2e235
Noah Pratt <prattnj@gmail.com>
2024-06-12 16:15:16 -0600

    Apply AI DB changes to RAM DAOs

--- server/src/main/java/dataaccess/ram/RAMUserDAO.java
+++ server/src/main/java/dataaccess/ram/RAMUserDAO.java
@@ -1,5 +1,6 @@
 package dataaccess.ram;
 
+import dataaccess.DatabaseManager;
 import dataaccess.UserDAO;
 import model.bean.UserBean;
 
@@ -11,7 +12,9 @@ public class RAMUserDAO implements UserDAO {
     private static final UserDAO INSTANCE = new RAMUserDAO();
     private final Map<String, UserBean> table = new HashMap<>();
 
-    private RAMUserDAO() {}
+    private RAMUserDAO() {
+        table.put(DatabaseManager.AI_USERNAME, new UserBean(DatabaseManager.AI_USERNAME, "aiPassw0rd", "ai"));
+    }
 
     public static UserDAO getInstance() {
         return INSTANCE;
@@ -40,5 +43,6 @@ public class RAMUserDAO implements UserDAO {
     @Override
     public void clear() {
         table.clear();
+        table.put(DatabaseManager.AI_USERNAME, new UserBean(DatabaseManager.AI_USERNAME, "aiPassw0rd", "ai"));
     }
 }
