d75189878c01211acf43229f1fe2ee71150f7f62
Noah Pratt <prattnj@gmail.com>
2024-06-12 18:53:18 -0600

    Add toString to ChessPosition

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -52,4 +52,9 @@ public class ChessPosition {
     public int hashCode() {
         return Objects.hash(row, col);
     }
+
+    @Override
+    public String toString() {
+        return Character.toString((char)('a' + (col - 1))) + row;
+    }
 }
