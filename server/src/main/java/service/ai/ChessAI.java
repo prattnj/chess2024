28676fb997ab8d2abc8bb04a4770d9f27e0dfbf0
Noah Pratt <prattnj@gmail.com>
2024-06-12 23:52:33 -0600

    Add untested minimax algorithm

--- server/src/main/java/service/ai/ChessAI.java
+++ server/src/main/java/service/ai/ChessAI.java
@@ -6,7 +6,37 @@ import model.AILevel;
 
 public class ChessAI {
 
-    public static ChessMove getMove(ChessGame game, ChessGame.TeamColor turn, AILevel level) {
+    Node root;
+    int maxDepth = 3;
+
+    public ChessMove getMove(ChessGame game, ChessGame.TeamColor turn, AILevel level) {
+        double bestValue = minimax(root, 1, true, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
         return null;
+    }
+
+    // minimax with alpha-beta pruning
+    private double minimax(Node n, int depth, boolean isMaxPlayer, double alpha, double beta) {
+        // leaf node (evaluation function)
+        if (depth == maxDepth) return n.evaluate();
+
+        if (isMaxPlayer) {
+            double max = Double.NEGATIVE_INFINITY;
+            for (Node child : n.getChildren()) {
+                double value = minimax(child, depth + 1, false, alpha, beta);
+                if (value > max) max = value;
+                if (max > alpha) alpha = max;
+                if (beta <= alpha) break;
+            }
+            return max;
+        } else {
+            double min = Double.POSITIVE_INFINITY;
+            for (Node child : n.getChildren()) {
+                double value = minimax(child, depth + 1, true, alpha, beta);
+                if (value < min) min = value;
+                if (min < beta) beta = min;
+                if (beta <= alpha) break;
+            }
+            return min;
+        }
     }
 }
