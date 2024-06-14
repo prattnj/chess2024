b991bcbf86f60f843904f3ebc6eaabab7d449f81
Noah Pratt <prattnj@gmail.com>
2024-06-14 13:54:31 -0600

    Reorganize ChessAI

--- server/src/main/java/service/ai/ChessAI.java
+++ server/src/main/java/service/ai/ChessAI.java
@@ -7,9 +7,8 @@ import util.Util;
 
 public class ChessAI {
 
-    private Node root;
     private int totalNodes;
-    private final static int MAX_DEPTH = 3;
+    private final static int MAX_DEPTH = 4;
     private final AILevel level;
     private ChessGame.TeamColor maxPlayer;
 
@@ -20,7 +19,9 @@ public class ChessAI {
     public ChessMove getMove(ChessGame game, ChessGame.TeamColor turn) {
         if (game.getTeamTurn() != turn) return null; // not the AI's turn
         maxPlayer = turn;
-        generateTree(game, turn);
+        totalNodes = 0;
+        Node root = new Node(game);
+        generateChildren(root, maxPlayer);
         double bestValue = Double.NEGATIVE_INFINITY;
         ChessMove bestMove = null;
         for (ChessMove m : root.getChildren().keySet()) {
@@ -34,33 +35,11 @@ public class ChessAI {
         return bestMove;
     }
 
-    private void generateTree(ChessGame game, ChessGame.TeamColor turn) {
-        System.gc(); // remind the JVM to free up heap space from the last tree
-        root = new Node(game);
-        totalNodes = 0;
-        generateChildren(root, maxPlayer, 1);
-    }
-
-    // recursive helper function for tree generation
-    private void generateChildren(Node n, ChessGame.TeamColor turn, int depth) {
-        if (depth > MAX_DEPTH) return;
-        for (ChessMove m : n.getGame().validMoves(turn)) {
-            ChessGame game = n.getGame().clone();
-            try {
-                game.makeMove(m);
-            } catch (Exception e) { // shouldn't happen
-                continue;
-            }
-            Node child = new Node(game);
-            n.addChild(m, child);
-            totalNodes++;
-            generateChildren(child, Util.oppositeColor(turn), depth + 1);
-        }
-    }
-
     // minimax with alpha-beta pruning
     private double minimax(Node n, int depth, ChessGame.TeamColor team, double alpha, double beta) {
-        if (n.getChildren().isEmpty()) return n.evaluate(team); // leaf node (evaluation function)
+        if (depth == MAX_DEPTH) return n.evaluate(team); // leaf node (evaluation function)
+
+        generateChildren(n, team);
 
         if (team == maxPlayer) {
             double max = Double.NEGATIVE_INFINITY;
@@ -80,6 +59,20 @@ public class ChessAI {
                 if (beta <= alpha) break;
             }
             return min;
+        }
+    }
+
+    private void generateChildren(Node n, ChessGame.TeamColor turn) {
+        for (ChessMove m : n.getGame().validMoves(turn)) {
+            ChessGame game = n.getGame().clone();
+            try {
+                game.makeMove(m);
+            } catch (Exception e) { // shouldn't happen
+                continue;
+            }
+            Node child = new Node(game);
+            n.addChild(m, child);
+            totalNodes++;
         }
     }
 }
