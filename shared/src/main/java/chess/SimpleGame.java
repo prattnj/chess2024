92154dd1b9ac416cf37cd1f7b14fab742cf42791
Noah Pratt <prattnj@gmail.com>
2024-06-17 12:15:01 -0600

    Bug fixing / refactoring SimpleGame

--- shared/src/main/java/chess/SimpleGame.java
+++ shared/src/main/java/chess/SimpleGame.java
@@ -18,11 +18,11 @@ public class SimpleGame {
     }
 
     public SimpleGame(ChessGame game) {
+        board = new char[8][8];
         for (int i = 1; i <= 8; i++) {
             for (int j = 1; j <= 8; j++) {
                 ChessPiece piece = game.getBoard().getPiece(new ChessPosition(i, j));
-                if (piece == null) board[i - 1][j - 1] = 0;
-                else board[i - 1][j - 1] = Util.getStringForPiece(piece).charAt(0);
+                if (piece != null) board[i - 1][j - 1] = Util.getStringForPiece(piece).charAt(0);
             }
         }
         this.turn = game.getTeamTurn() == ChessGame.TeamColor.WHITE ? 1 : 2;
@@ -31,31 +31,39 @@ public class SimpleGame {
 
     // example move: 0,1,3,3 == move from b1 to d4
     public LinkedList<int[]> validMoves(int team) {
-        LinkedList<int[]> pieceMoves;
         LinkedList<int[]> moves = new LinkedList<>();
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 if (board[i][j] == 0) continue;
                 int color = Character.isLowerCase(board[i][j]) ? 2 : 1;
                 if (color != team) continue;
-                pieceMoves = switch (Util.getTypeForChar(board[i][j])) {
-                    case KING -> kingMoves(i, j, color);
-                    case QUEEN -> queenMoves(i, j, color);
-                    case ROOK -> rookMoves(i, j, color);
-                    case KNIGHT -> knightMoves(i, j, color);
-                    case BISHOP -> bishopMoves(i, j, color);
-                    case PAWN -> pawnMoves(i, j, color);
-                };
-                for (int[] move : pieceMoves) {
-                    // make move, in check?, undo move
-                    char mover = board[move[0]][move[1]];
-                    char victim = board[move[2]][move[3]];
-                    updateBoard(move, (char)0, mover, true);
-                    boolean inCheck = inCheck(team);
-                    updateBoard(move, mover, victim, false);
-                    if (!inCheck) moves.add(move);
-                }
+                moves.addAll(validMoves(i, j));
             }
+        }
+        return moves;
+    }
+
+    public LinkedList<int[]> validMoves(int row, int col) {
+        LinkedList<int[]> pieceMoves;
+        LinkedList<int[]> moves = new LinkedList<>();
+        if (board[row][col] == 0) return moves;
+        int color = Character.isLowerCase(board[row][col]) ? 2 : 1;
+        pieceMoves = switch (Util.getTypeForChar(board[row][col])) {
+            case KING -> kingMoves(board, row, col);
+            case QUEEN -> queenMoves(board, row, col);
+            case ROOK -> rookMoves(board, row, col);
+            case KNIGHT -> knightMoves(board, row, col);
+            case BISHOP -> bishopMoves(board, row, col);
+            case PAWN -> pawnMoves(board, row, col);
+        };
+        for (int[] move : pieceMoves) {
+            // make move, in check?, undo move
+            char mover = board[move[0]][move[1]];
+            char victim = board[move[2]][move[3]];
+            updateBoard(move, (char)0, mover, true);
+            boolean inCheck = inCheck(color);
+            updateBoard(move, mover, victim, false);
+            if (!inCheck) moves.add(move);
         }
         return moves;
     }
@@ -174,8 +182,34 @@ public class SimpleGame {
         return false;
     }
 
+    public int countInstances(char c) {
+        int counter = 0;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (board[i][j] == c) counter++;
+            }
+        }
+        return counter;
+    }
+
+    public char[][] getBoard() {
+        return board;
+    }
+
+    public int getTurn() {
+        return turn;
+    }
+
     public boolean isOngoing() {
         return ongoing;
+    }
+
+    public SimpleGame clone() {
+        SimpleGame clone = new SimpleGame();
+        for (int i = 0; i < 8; i++) System.arraycopy(board[i], 0, clone.board[i], 0, 8);
+        clone.turn = turn;
+        clone.ongoing = ongoing;
+        return clone;
     }
 
     private void resetBoard() {
@@ -217,31 +251,37 @@ public class SimpleGame {
         }
     }
 
-    private LinkedList<int[]> kingMoves(int row, int col, int team) {
+    public static LinkedList<int[]> kingMoves(char[][] board, int row, int col) {
         LinkedList<int[]> moves = new LinkedList<>();
+        int team = Character.isLowerCase(board[row][col]) ? 2 : 1;
         for (int i = -1; i <= 1; i++) {
             for (int j = -1; j <= 1; j++) {
                 int newRow = row + i;
                 int newCol = col + j;
-                if (validatePosition(newRow, newCol, team)) moves.add(new int[]{row, col, newRow, newCol});
+                if (validatePosition(board, newRow, newCol, team)) moves.add(new int[]{row, col, newRow, newCol});
             }
         }
         return moves;
     }
 
-    private LinkedList<int[]> queenMoves(int row, int col, int team) {
-        LinkedList<int[]> moves = rookMoves(row, col, team);
-        moves.addAll(bishopMoves(row, col, team));
+    public static LinkedList<int[]> queenMoves(char[][] board, int row, int col) {
+        int team = Character.isLowerCase(board[row][col]) ? 2 : 1;
+        LinkedList<int[]> moves = rookMoves(board, row, col);
+        moves.addAll(bishopMoves(board, row, col));
         return moves;
     }
 
-    private LinkedList<int[]> rookMoves(int row, int col, int team) {
+    public static LinkedList<int[]> rookMoves(char[][] board, int row, int col) {
         LinkedList<int[]> moves = new LinkedList<>();
+        int team = Character.isLowerCase(board[row][col]) ? 2 : 1;
 
         int d = 1;
         while(true) {
             int newRow = row + d;
-            if (validatePosition(newRow, col, team)) moves.add(new int[]{row, col, newRow, col});
+            if (validatePosition(board, newRow, col, team)) {
+                moves.add(new int[]{row, col, newRow, col});
+                if (board[newRow][col] != 0) break;
+            }
             else break;
             d++;
         }
@@ -249,7 +289,10 @@ public class SimpleGame {
         d = 1;
         while(true) {
             int newRow = row - d;
-            if (validatePosition(newRow, col, team)) moves.add(new int[]{row, col, newRow, col});
+            if (validatePosition(board, newRow, col, team)) {
+                moves.add(new int[]{row, col, newRow, col});
+                if (board[newRow][col] != 0) break;
+            }
             else break;
             d++;
         }
@@ -257,7 +300,10 @@ public class SimpleGame {
         d = 1;
         while(true) {
             int newCol = col + d;
-            if (validatePosition(row, newCol, team)) moves.add(new int[]{row, col, row, newCol});
+            if (validatePosition(board, row, newCol, team)) {
+                moves.add(new int[]{row, col, row, newCol});
+                if (board[row][newCol] != 0) break;
+            }
             else break;
             d++;
         }
@@ -265,7 +311,10 @@ public class SimpleGame {
         d = 1;
         while(true) {
             int newCol = col - d;
-            if (validatePosition(row, newCol, team)) moves.add(new int[]{row, col, row, newCol});
+            if (validatePosition(board, row, newCol, team)) {
+                moves.add(new int[]{row, col, row, newCol});
+                if (board[row][newCol] != 0) break;
+            }
             else break;
             d++;
         }
@@ -273,52 +322,57 @@ public class SimpleGame {
         return moves;
     }
 
-    private LinkedList<int[]> knightMoves(int row, int col, int team) {
+    public static LinkedList<int[]> knightMoves(char[][] board, int row, int col) {
         LinkedList<int[]> moves = new LinkedList<>();
+        int team = Character.isLowerCase(board[row][col]) ? 2 : 1;
 
         int newRow = row + 1;
         int newCol = col + 2;
-        if (validatePosition(newRow, newCol, team)) moves.add(new int[]{row, col, newRow, newCol});
+        if (validatePosition(board, newRow, newCol, team)) moves.add(new int[]{row, col, newRow, newCol});
 
         newRow = row + 2;
         newCol = col + 1;
-        if (validatePosition(newRow, newCol, team)) moves.add(new int[]{row, col, newRow, newCol});
+        if (validatePosition(board, newRow, newCol, team)) moves.add(new int[]{row, col, newRow, newCol});
 
         newRow = row + 1;
         newCol = col - 2;
-        if (validatePosition(newRow, newCol, team)) moves.add(new int[]{row, col, newRow, newCol});
+        if (validatePosition(board, newRow, newCol, team)) moves.add(new int[]{row, col, newRow, newCol});
 
         newRow = row + 2;
         newCol = col - 1;
-        if (validatePosition(newRow, newCol, team)) moves.add(new int[]{row, col, newRow, newCol});
+        if (validatePosition(board, newRow, newCol, team)) moves.add(new int[]{row, col, newRow, newCol});
 
         newRow = row - 1;
         newCol = col + 2;
-        if (validatePosition(newRow, newCol, team)) moves.add(new int[]{row, col, newRow, newCol});
+        if (validatePosition(board, newRow, newCol, team)) moves.add(new int[]{row, col, newRow, newCol});
 
         newRow = row - 2;
         newCol = col + 1;
-        if (validatePosition(newRow, newCol, team)) moves.add(new int[]{row, col, newRow, newCol});
+        if (validatePosition(board, newRow, newCol, team)) moves.add(new int[]{row, col, newRow, newCol});
 
         newRow = row - 1;
         newCol = col - 2;
-        if (validatePosition(newRow, newCol, team)) moves.add(new int[]{row, col, newRow, newCol});
+        if (validatePosition(board, newRow, newCol, team)) moves.add(new int[]{row, col, newRow, newCol});
 
         newRow = row - 2;
         newCol = col - 1;
-        if (validatePosition(newRow, newCol, team)) moves.add(new int[]{row, col, newRow, newCol});
+        if (validatePosition(board, newRow, newCol, team)) moves.add(new int[]{row, col, newRow, newCol});
 
         return moves;
     }
 
-    private LinkedList<int[]> bishopMoves(int row, int col, int team) {
+    public static LinkedList<int[]> bishopMoves(char[][] board, int row, int col) {
         LinkedList<int[]> moves = new LinkedList<>();
+        int team = Character.isLowerCase(board[row][col]) ? 2 : 1;
 
         int d = 1;
         while(true) {
             int newRow = row + d;
             int newCol = col + d;
-            if (validatePosition(newRow, newCol, team)) moves.add(new int[]{row, col, newRow, newCol});
+            if (validatePosition(board, newRow, newCol, team)) {
+                moves.add(new int[]{row, col, newRow, newCol});
+                if (board[newRow][newCol] != 0) break;
+            }
             else break;
             d++;
         }
@@ -327,7 +381,10 @@ public class SimpleGame {
         while(true) {
             int newRow = row + d;
             int newCol = col - d;
-            if (validatePosition(newRow, newCol, team)) moves.add(new int[]{row, col, newRow, newCol});
+            if (validatePosition(board, newRow, newCol, team)) {
+                moves.add(new int[]{row, col, newRow, newCol});
+                if (board[newRow][newCol] != 0) break;
+            }
             else break;
             d++;
         }
@@ -336,7 +393,10 @@ public class SimpleGame {
         while(true) {
             int newRow = row - d;
             int newCol = col + d;
-            if (validatePosition(newRow, newCol, team)) moves.add(new int[]{row, col, newRow, newCol});
+            if (validatePosition(board, newRow, newCol, team)) {
+                moves.add(new int[]{row, col, newRow, newCol});
+                if (board[newRow][newCol] != 0) break;
+            }
             else break;
             d++;
         }
@@ -345,7 +405,10 @@ public class SimpleGame {
         while(true) {
             int newRow = row - d;
             int newCol = col - d;
-            if (validatePosition(newRow, newCol, team)) moves.add(new int[]{row, col, newRow, newCol});
+            if (validatePosition(board, newRow, newCol, team)) {
+                moves.add(new int[]{row, col, newRow, newCol});
+                if (board[newRow][newCol] != 0) break;
+            }
             else break;
             d++;
         }
@@ -353,8 +416,9 @@ public class SimpleGame {
         return moves;
     }
 
-    private LinkedList<int[]> pawnMoves(int row, int col, int team) {
+    public static LinkedList<int[]> pawnMoves(char[][] board, int row, int col) {
         LinkedList<int[]> moves = new LinkedList<>();
+        int team = Character.isLowerCase(board[row][col]) ? 2 : 1;
 
         int doubleRow = team == 1 ? 1 : 6;
         int promoRow = 7 - doubleRow;
@@ -362,11 +426,12 @@ public class SimpleGame {
         int newRow = row + d;
 
         // forward 1, diagonals
-        pawnHelper(moves, row, col, team, false);
+        if (row != promoRow)
+            pawnHelper(board, moves, row, col, team, false);
 
         // promos
         if (row == promoRow)
-            pawnHelper(moves, row, col, team, true);
+            pawnHelper(board, moves, row, col, team, true);
 
         // forward 2
         if (row == doubleRow) {
@@ -377,7 +442,7 @@ public class SimpleGame {
         return moves;
     }
 
-    private void pawnHelper(LinkedList<int[]> moves, int row, int col, int team, boolean promo) {
+    private static void pawnHelper(char[][] board, LinkedList<int[]> moves, int row, int col, int team, boolean promo) {
         int d = team == 1 ? 1 : -1;
         int newRow = row + d;
         ChessPiece.PieceType[] promos = new ChessPiece.PieceType[]{ChessPiece.PieceType.QUEEN, ChessPiece.PieceType.ROOK, ChessPiece.PieceType.KNIGHT, ChessPiece.PieceType.BISHOP};
@@ -414,14 +479,14 @@ public class SimpleGame {
     }
 
     // see if a piece can move to the location specified (not applicable to pawns)
-    private boolean validatePosition(int row, int col, int moverTeam) {
+    private static boolean validatePosition(char[][] board, int row, int col, int moverTeam) {
         if (!positionInBounds(row, col)) return false;
         if (board[row][col] == 0) return true;
         int victimTeam = Character.isLowerCase(board[row][col]) ? 2 : 1;
         return moverTeam != victimTeam;
     }
 
-    private boolean positionInBounds(int row, int col) {
+    private static boolean positionInBounds(int row, int col) {
         return row >= 0 && row <= 7 && col >= 0 && col <= 7;
     }
 
@@ -433,11 +498,5 @@ public class SimpleGame {
             }
         }
         return null;
-    }
-
-    public static void main(String[] args) {
-        SimpleGame sg = new SimpleGame();
-        //sg.makeMove(new int[]{1, 0, 3, 0});
-        System.out.println(sg.validMoves(1).size());
     }
 }
