cc19e7633677252da8dbc5dd3536e669e4f51bcf
Noah Pratt <prattnj@gmail.com>
2024-06-17 12:14:48 -0600

    Add tests for SimpleGame functionality

---
+++ shared/src/test/java/passoff/chess/simple/SimpleGameTests.java
@@ -0,0 +1,29 @@
+package passoff.chess.simple;
+
+import chess.SimpleGame;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+
+public class SimpleGameTests {
+
+    private SimpleGame sg;
+
+    @BeforeEach
+    public void setup() {
+        sg = new SimpleGame();
+    }
+
+    @Test
+    public void testMakeMove() {
+        sg.makeMove(new int[]{1, 0, 3, 0});
+        System.out.println();
+    }
+
+    @Test
+    public void testValidMoves() {
+        LinkedList<int[]> moves = sg.validMoves(1);
+        System.out.println();
+    }
+}
